bc2299afa11cf6b8abe01266556001e777721550
naveen <naveendot1q@gmail.com>
2026-07-01 11:19:48 +0530

    v2-real

--- next.config.js
+++ next.config.js
@@ -1,21 +1,33 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/middleware-manifest\.json$/],
-});
+// @ts-check
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
       'localhost',
-      '*.supabase.co',
       'lh3.googleusercontent.com',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require('@ducanh2912/next-pwa').default({
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+      disable: phase === PHASE_DEVELOPMENT_SERVER,
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
